8870eb1e05c787b6fb63bd8f1aaa3e546a648016
Gabriel <gabriel_silva@discente.ufg.br>
2020-09-04 10:43:15 -0300

    adição de feedback visual

--- src/index.js
+++ src/index.js
@@ -1,15 +1,24 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import { render } from 'react-dom';
 import App from './App';
 import FacebookLogin from 'react-facebook-login';
 
 const ReactFacebookLogin = () => {
 
-  const [accessToken, setAccessToken] = useState("");
+  const [authenticated, setAuthenticated] = useState(false);
+
+  const [profilePicture, setProfilePicture] = useState("");
+  const [profileName, setProfileName] = useState("");
+  const [profileEmail, setProfileEmail] = useState("");
+
 
   const responseFacebook = (response) => {
     console.log(response);
-    setAccessToken(response.accessToken);
+    setProfileName(response.name);
+    setProfilePicture(response.picture.data.url);
+    setProfileEmail(response.email);
+    setAuthenticated(true);
+
   }
 
   const componentClicked = (response) => {
@@ -17,17 +26,28 @@ const ReactFacebookLogin = () => {
   }
 
   return (
-    <div>
+    <div style={{ display: 'flex', flexDirection: 'column', flexGrow: 1, justifyContent: 'center', alignItems: 'center' }}>
       <App />
-      {accessToken}
-      <FacebookLogin
-        appId="1643380219164794"
-        autoLoad={true}
-        fields="name,email,picture"
-        onClick={componentClicked}
-        callback={responseFacebook}
-        textButton="Login com Facebook"
-      />
+
+      {authenticated ? (
+        <div style={{ backgroundColor: '#d4d4d4', height: 250, width: 250, display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+          <img src={profilePicture} alt={profileName} style={{ height: 50, borderRadius: '50%' }} />
+          <div>
+            <h3>{profileName}</h3>
+            <p>{profileEmail}</p>
+          </div>
+        </div>
+      ) : (
+          <FacebookLogin
+            appId="1643380219164794"
+            autoLoad={true}
+            fields="name,email,picture"
+            onClick={componentClicked}
+            callback={responseFacebook}
+            textButton="Login com Facebook"
+          />
+        )}
+
     </div>
   );
 }
